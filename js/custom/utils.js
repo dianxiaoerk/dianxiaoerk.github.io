051713a714b50bb1a6f39d8ea9f24375cda359f1
zzk <dianxiaoerk@163.com>
2020-05-21 18:27:54 +0800

    Site updated: 2020-05-21 18:27:53

---
+++ js/custom/utils.js
@@ -0,0 +1,14 @@
+$(function(){
+  debugger
+  //相关书籍点击查看
+  $('.show-books .icon').click(function(){
+    var flag = $(this).hasClass('icon-right');
+    if(flag){
+      $(this).addClass('icon-down').removeClass('icon-right')
+
+    }else{
+      $(this).addClass('icon-right').removeClass('icon-down')
+    }
+    $('.popular-posts').slideToggle()
+  })
+})
